e40c8015f0ced44388ad4671327e865180d245c4
Chris Hough <chris@noconformity.com>
2013-06-11 12:09:55 -0700

    completely re-write

--- lib/jquery_rails_google_cdn/railtie.rb
+++ lib/jquery_rails_google_cdn/railtie.rb
@@ -1,12 +1,12 @@
-# lib/my_gem/railtie.rb
-puts "I AM LOADING"
-
-module Jquery::Rails::Google::CDN
-  class Railtie < Rails::Railtie
-    initializer "jquery_rails_google_cdn.view_helpers" do
-      Rails.logger.debug "OH HI I'M INJECTING YOU WITH MY SEED"
-      puts "FUCK "
-      ActionView::Base.send :include, Jquery::Rails::Google::CDN::ViewHelpers
+module Jquery::Rails
+  module Google
+    module CDN
+      class Railtie < Rails::Railtie
+        initializer "jquery_rails_google_cdn.view_helpers" do
+          Rails.logger.debug "OH HI I'M INJECTING YOU WITH MY SEED"
+          ActionView::Base.send :include, Jquery::Rails::Google::CDN::ViewHelpers
+        end
+      end
     end
   end
 end
